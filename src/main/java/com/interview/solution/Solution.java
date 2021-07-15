a705569301805e82f2cc2c9b47bc831cbba8488b
liushaogeng <liushaogeng@baidu.com>
2021-07-15 10:16:25 +0800

    数组中的元素向右移动 k 个

--- src/main/java/com/interview/solution/Solution.java
+++ src/main/java/com/interview/solution/Solution.java
@@ -3,6 +3,25 @@ package com.interview.solution;
 import java.util.Stack;
 
 public class Solution {
+
+    /**
+     * create by shaogeng at 2021/7/13 09:46
+     * description: 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     * 输入: nums = [1,2,3,4,5,6,7], k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右旋转 1 步: [7,1,2,3,4,5,6]
+     * 向右旋转 2 步: [6,7,1,2,3,4,5]
+     * 向右旋转 3 步: [5,6,7,1,2,3,4]
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public void rotate(int[] nums, int k) {
+
+    }
+
     /**
      * create by shaogeng at 2021/7/13 19:38
      * description: 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
