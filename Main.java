a355458c8ea6777fa9e542dc918947a17f6e7035
paceboy <paceboy@163.com>
2021-08-15 16:06:25 +0800

    Add files via upload
    统计字符串个数

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.interview.solution;
+
+/**
+ * @author : liushaogeng
+ * create at:  2021/7/13
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        // System.out.printf("searchInsert = %d \n", s.searchInsert(new int[]{-1,0,3,5,9,12}, 9));
+        // System.out.printf("searchInsert = %d \n", s.searchInsert(new int[]{1,3,5,6}, 2));
+        // System.out.printf("searchInsert = %d \n", s.searchInsert(new int[]{1,3,5,6}, 0));
+        // System.out.printf("searchInsert = %d \n", s.searchInsert(new int[]{1}, 1));
+//        System.out.printf("searchInsert = %d \n", s.searchInsert(new int[]{1,3}, 3));
+
+        s.statisticsWordCount("face face is big big small big");
+    }
+}
